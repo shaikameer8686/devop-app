2ec5bae7c95720f5ec333c7f5c4948602c3eb296
shaikameer8686 <83751103+shaikameer8686@users.noreply.github.com>
2021-07-11 11:34:35 +0530

    Update App.java

--- src/main/java/com/vikas/app/App.java
+++ src/main/java/com/vikas/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from jenkins and integrating with Maven" );
+        System.out.println( "Hello World from jenkins and integrating with Maven making changes in Webhook" );
     }
 }
